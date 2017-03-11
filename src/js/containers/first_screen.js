6779ab4b745fd6e8e65516f6c9b6dc6a8203ec59
SiDevesh <sid.swapnildevesh@gmail.com>
2017-03-11 10:52:12 +0530

    Edit first screen

--- src/js/containers/first_screen.js
+++ src/js/containers/first_screen.js
@@ -12,7 +12,7 @@ export default class FirstScreen extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to Settings!
+          Welcome to first!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
